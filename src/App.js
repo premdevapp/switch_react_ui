9eb4ccc1ad26fe0d378fcd01642564ae2930bab4
premnath .p <57990514+premdevapp@users.noreply.github.com>
2021-03-04 06:22:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+import NewsletterStates from "./containers/NewsletterStates";
+
+export default function App() {
+  return (
+    <div className="container">
+      <NewsletterStates />
+    </div>
+  );
+}
